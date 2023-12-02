15610c19dca230b6a04eacda0fd3a1076b00a967
Luca <lucaan@ethz.ch>
2023-12-02 20:06:57 +0100

    added day 2

---
+++ day-02/src/bin/part1.rs
@@ -0,0 +1,34 @@
+use std::collections::HashMap;
+
+fn main() {
+    let input = include_str!("./input1.txt");
+    let output = part1(input);
+    dbg!(output);
+}
+
+fn part1(input: &str) -> i32 {
+    let mut lines = input.lines();
+    let mut balls = HashMap::new();
+    balls.insert("red", "12");
+    balls.insert("blue", "14");
+    balls.insert("green", "13");
+    while let Some(line) = lines.next() {}
+}
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = part1(
+            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+            Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+            Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+            Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+            Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+        );
+        assert_eq!(result, 8);
+    }
+}
---
+++ day-02/src/bin/part2.rs
@@ -0,0 +1,74 @@
+use std::str;
+
+fn main() {
+    let input = include_str!("./input2.txt");
+    let output = part2(input);
+    dbg!(output);
+}
+
+fn part2(input: &str) -> i32 {
+    let numbers_spelled_out_with_letters = [
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+
+    input
+        .lines()
+        .map(|line| {
+            let mut characters_digits: Vec<char> = vec![];
+            let mut temporary = String::from("");
+            for character in line.chars() {
+                temporary += &character.to_string();
+
+                let mut temporary_spelled_number_index = None;
+                for (index, spelled_number) in numbers_spelled_out_with_letters.iter().enumerate() {
+                    if temporary.contains(spelled_number) {
+                        temporary_spelled_number_index = Some(index);
+                        break;
+                    }
+                }
+                if let Some(temporary_spelled_number_index) = temporary_spelled_number_index {
+                    let number = temporary_spelled_number_index + 1;
+                    characters_digits.push(
+                        number
+                            .to_string()
+                            .chars()
+                            .next()
+                            .expect("Number should be single-character digit."),
+                    );
+                    temporary = character.to_string();
+                }
+
+                if character.is_ascii_digit() {
+                    characters_digits.push(character);
+                    temporary = String::from("");
+                }
+            }
+
+            let first_digit = characters_digits.first().unwrap_or(&'0').to_owned();
+            let last_digit = characters_digits.last().unwrap_or(&'0').to_owned();
+            let number = format!("{}{}", first_digit, last_digit);
+            let number: i32 = number.parse().expect("Should parse as a number.");
+            number
+        })
+        .sum()
+}
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = part2(
+            "two1nine
+            eightwothree
+            abcone2threexyz
+            xtwone3four
+            4nineeightseven2
+            zoneight234
+            7pqrstsixteen",
+        );
+        assert_eq!(result, 281);
+    }
+}
