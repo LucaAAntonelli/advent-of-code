ba5df3b2cff65c9d59d8fc872bef7c7f623d7ae0
Luca <lucaan@ethz.ch>
2023-12-02 10:11:38 +0100

    set up part 2

--- day-01/src/bin/part2.rs
+++ day-01/src/bin/part2.rs
@@ -1,1 +1,52 @@
+use std::str;
 
+fn main() {
+    let input = include_str!("./input2.txt");
+    let output = part2(input);
+    dbg!(output);
+}
+
+fn part2(input: &str) -> String {
+    let mut lines = input.lines();
+    let mut sum = 0;
+    let numeric_values: Vec<char> = "0123456789".chars().collect();
+    while let Some(line) = lines.next() {
+        // Find first numeric value
+        let first = line.find(|x| numeric_values.contains(&x)).unwrap();
+        // Find last numeric value
+        let last = line.rfind(|x| numeric_values.contains(&x)).unwrap();
+        // Combine to string
+
+        let calibration_value = format!(
+            "{}{}",
+            str::from_utf8(&vec![line.as_bytes()[first]]).unwrap(),
+            str::from_utf8(&vec![line.as_bytes()[last]]).unwrap()
+        );
+        println!("{calibration_value}");
+        // Parse to int
+        sum += calibration_value.parse::<i32>().unwrap();
+        // Add to sum
+    }
+
+    sum.to_string()
+}
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = part2(
+            "two1nine
+            eightwothree
+            abcone2threexyz
+            xtwone3four
+            4nineeightseven2
+            zoneight234
+            7pqrstsixteen",
+        );
+        assert_eq!(result, "281");
+    }
+}
--- day-01/target/debug/deps/part2-693bf6c432d15c9e.d
+++ day-01/target/debug/deps/part2-693bf6c432d15c9e.d
@@ -1,5 +1,6 @@
-/home/luca/Projects/Rust/advent-of-code/day-01/target/debug/deps/part2-693bf6c432d15c9e.rmeta: src/bin/part2.rs
+/home/luca/Projects/Rust/advent-of-code/day-01/target/debug/deps/part2-693bf6c432d15c9e.rmeta: src/bin/part2.rs src/bin/./input2.txt
 
-/home/luca/Projects/Rust/advent-of-code/day-01/target/debug/deps/part2-693bf6c432d15c9e.d: src/bin/part2.rs
+/home/luca/Projects/Rust/advent-of-code/day-01/target/debug/deps/part2-693bf6c432d15c9e.d: src/bin/part2.rs src/bin/./input2.txt
 
 src/bin/part2.rs:
+src/bin/./input2.txt:
